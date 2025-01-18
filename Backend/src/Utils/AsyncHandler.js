8feffa4e6381d69335e9f7c7b8ea43eb0a2ccd5f
Sanket <Sanketpanchal775@gmail.com>
2025-01-18 19:39:27 +0530

    Deleted Backend Folder for confedentiality

--- Backend/src/Utils/AsyncHandler.js
+++
@@ -1,3 +0,0 @@
-export default AsyncHandler = (fn) => {
-    return (req , res ,next) => { Promise.resolve(fn(req , res ,next)).catch(err => next(err)) }
-}
--- Backend/src/constants.js
+++
@@ -1,1 +0,0 @@
-export const DataBase_Name = 'Trip-Planner-Ai'
